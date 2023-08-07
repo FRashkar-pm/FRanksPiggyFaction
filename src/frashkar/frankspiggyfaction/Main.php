abc4f1e635ad3a8c7c3d52896b7fc3cd3220a1f6
FRashkar-pm <nicholassaputra847@gmail.com>
2023-08-07 10:51:55 +0700

    Simple !

---
+++ src/frashkar/frankspiggyfaction/Main.php
@@ -0,0 +1,82 @@
+<?php
+
+/**
+*  ______ _____           _     _                                     
+* |  ____|  __ \         | |   | |                                    
+* | |__  | |__) |__ _ ___| |__ | | ____ _ _ __ ______ _ __  _ __ ___  
+* |  __| |  _  // _` / __| '_ \| |/ / _` | '__|______| '_ \| '_ ` _ \ 
+* | |    | | \ \ (_| \__ \ | | |   < (_| | |         | |_) | | | | | |
+* |_|    |_|  \_\__,_|___/_| |_|_|\_\__,_|_|         | .__/|_| |_| |_|
+*                                                    | |              
+*                                                    |_|              
+* The author of this plugin is FRashkar-pm
+* https://github.com/FRashkar-pm
+* Discord: FireRashkar#1519
+*/
+
+declare(strict_types=1);
+
+namespace frashkar\frankspiggyfaction;
+
+use IvanCraft623\RankSystem\RankSystem;
+use IvanCraft623\RankSystem\session\Session;
+use IvanCraft623\RankSystem\tag\Tag;
+use DaPigGuy\PiggyFactions\PiggyFactions;
+use pocketmine\player\Player;
+use pocketmine\plugin\PluginBase;
+use pocketmine\Server;
+
+class Main extends PluginBase
+{
+    protected function onEnable() : void 
+    {
+        if (self::getRankSystem() === null)
+        {
+            $this->getLogger()->notice("There are no RankSystem plugin.");
+            $this->disablePlugin($this);
+        }
+        if (self::getPiggyFaction() === null)
+        {
+            $this->getLogger()->info("There are no PiggyFactions plugin.");
+            $this->disablePlugin($this);
+        }
+        $this->setPiggyFactionSupport();
+    }
+
+    private static function getRankSystem() : ?RankSystem
+    {
+        return Server::getInstance()->getPluginManager()->getPlugin("RankSystem");
+    }
+
+    private static function getPiggyFaction() : ?PiggyFactions
+    {
+        return Server::getInstance()->getPluginManager()->getPlugin("PiggyFactions");
+    }
+
+    private static function getPlayerFaction(Player $player) : string 
+    {
+		$piggyFactions = self::getPiggyFaction();
+		$member = $piggyFactions->getPlayerManager()->getPlayer($player);
+		if ($member === null) return "";
+		if ($member !== null)
+		{
+			$faction = $member->getFaction();
+			if ($faction === null) return "";
+			return $faction->getName();
+		}
+	}
+
+    private function setPiggyFactionSupport() : void 
+    {
+        $rankSystem = self::getRankSystem();
+        $tagManager = $rankSystem->getTagManager();
+        $tagManager->registerTag(new Tag("fac_name", static function(Session $user) : string {
+			$player = $user->getPlayer();
+			if ($player === null) return "";
+			if ($player instanceof Player)
+			{
+				return self::getPlayerFaction($player);
+			}
+		}));
+    }
+}
